f2e2457ec5e3e7839230a018cb05e78c3eb0596a
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-05-18 22:30:00 +0500

    Add files via upload

---
+++ React-Level-6/26_UseEffect-Hook-Cleanup/cleaning-hook/src/components/PostList.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    setFetching(true);
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      });
+
+    return () => {
+      console.log("cleaning up UseEffect.");
+    };
+  }, []);
+
+  return (
+    <>
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
+    </>
+  );
+};
+
+export default PostList;
