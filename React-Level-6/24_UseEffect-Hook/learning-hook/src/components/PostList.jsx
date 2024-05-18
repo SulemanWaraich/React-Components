ef6257d1a2efa37686c9a07cfe2644bf9e4398ed
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-05-18 22:26:48 +0500

    Add files via upload

---
+++ React-Level-6/24_UseEffect-Hook/learning-hook/src/components/PostList.jsx
@@ -0,0 +1,24 @@
+import { useContext, useEffect } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListData);
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => addInitialPosts(data.posts));
+  }, []);
+
+  return (
+    <>
+      {postList.length === 0 && <WelcomeMessage />}
+      {postList.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostList;
---
+++ React-Level-6/24_UseEffect-Hook/learning-hook/src/components/WelcomeMessage.jsx
@@ -0,0 +1,9 @@
+const WelcomeMessage = () => {
+  return (
+    <center className="welcome">
+      <h1>There Are No Posts</h1>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
